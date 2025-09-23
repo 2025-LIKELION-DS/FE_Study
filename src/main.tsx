6a5f8a41a178cf6ce8ce69c93caad5331165f9f4
zer0p01nt <1027.bmy@gmail.com>
2025-09-24 01:42:46 +0900

    Chore: Tanstack Query 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.tsx";
 
+export const qc = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <QueryClientProvider client={qc}>
     <App />
-  </StrictMode>
+    {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+  </QueryClientProvider>
 );
